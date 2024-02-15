ad47dba7da855cf1368c1a343be743eb6f2467e9
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-15 20:47:10 -0300

    ajustes

--- Lista1/Exercicio12/src/Main.java
+++ Lista1/Exercicio12/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double n1 ,n2 ,resultn1, resultn2, result, resultf;
+        double n1 ,n2 ,media;
         Scanner in = new Scanner(System.in);
         System.out.println("Digite a primeira nota: ");
 
@@ -12,15 +12,9 @@ public class Main {
 
         n2 = in.nextDouble();
 
-        resultn1 = n1 * 1;
+        media = (n1 + n2*2)/3;
 
-        resultn2 = n2 * 2;
-
-        result = resultn1 + resultn2;
-
-        resultf = result / 3;
-
-        System.out.println("A média da nota é: " + resultf);
+        System.out.println("A média da nota é: " + media);
 
 
     }
