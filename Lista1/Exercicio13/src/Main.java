3cd65907a4d86853d651cab369d00ff973404092
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-15 20:54:45 -0300

    ajustes

--- Lista1/Exercicio13/src/Main.java
+++ Lista1/Exercicio13/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double raio, rr, result;
+        double raio, rr;
         Scanner in = new Scanner(System.in);
         System.out.println("Digite o raio do circulo: ");
 
