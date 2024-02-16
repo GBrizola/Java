e4ed8354149b09c533345b32141e7f882648c08f
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-15 21:09:55 -0300

    ajustes

--- Lista1/Exercicio17/src/Main.java
+++ Lista1/Exercicio17/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        double velIni, acel, temp, result, conta;
+        double velIni, acel, temp, result;
         Scanner in = new Scanner(System.in);
         System.out.println("Digite a velocidade inicial:");
 
@@ -16,9 +16,7 @@ public class Main {
 
         temp = in.nextDouble();
 
-        conta = acel * temp;
-        result = velIni + conta;
-
+        result = velIni + acel * temp;
 
         System.out.println("A sua velocidade final é: " + result);
 
