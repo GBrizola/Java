ab30e7ef144a138ea1cb62cc348ab63623fbf667
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-15 22:19:00 -0300

    ajustes

--- Lista1/Exercicio20/src/Main.java
+++ Lista1/Exercicio20/src/Main.java
@@ -5,10 +5,10 @@ public class Main {
         double b, c, x1, x2;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Digite o valor de X1: ");
+        System.out.println("Digite o valor de x1: ");
         x1 = in.nextDouble();
 
-        System.out.println("Digite o valor de X2: ");
+        System.out.println("Digite o valor de x2: ");
         x2 = in.nextDouble();
 
         b = -(x1 + x2);
