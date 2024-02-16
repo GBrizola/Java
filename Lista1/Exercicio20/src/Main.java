4b32ede2f9fc8bfab1b6305ebcc6030f5b153191
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-15 22:18:01 -0300

    ajustes

---
+++ Lista1/Exercicio20/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        double b, c, x1, x2;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Digite o valor de X1: ");
+        x1 = in.nextDouble();
+
+        System.out.println("Digite o valor de X2: ");
+        x2 = in.nextDouble();
+
+        b = -(x1 + x2);
+        c = x1 + x2;
+
+        System.out.println("A equação a partir de duas raízes é: x² + (" + b + "x) + (" + c + ") = 0");
+    }
+}
