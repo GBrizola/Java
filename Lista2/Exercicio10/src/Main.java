edbe0df0af7ae6602c5535493a6ee7ff4fae93bb
Gabriel Brizola <gabrielbrizola13@gmail.com>
2024-02-29 21:02:12 -0300

    Correção ex9

--- Lista2/Exercicio10/src/Main.java
+++ Lista2/Exercicio10/src/Main.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int menor, meio, maior, aux;
         Scanner in = new Scanner(System.in);
-        int num1, num2, num3;
+        System.out.println("Digite 3 inteiros");
+        menor = in.nextInt();
+        meio = in.nextInt();
+        maior = in.nextInt();
 
-        System.out.println("Digite três números inteiros para ordená-los crescentemente: ");
-        num1 = in.nextInt();
-        num2 = in.nextInt();
-        num3 = in.nextInt();
-
-        if (num1 < num2 & num2 < num3) {
-            System.out.println("Ordem dos números: " + num1 +", "+ num2 +", "+ num3);
-        } else if (num1 < num3 & num3 < num2) {
-            System.out.println("Ordem dos números: " + num1 +", "+ num3 +", "+ num2);
-        } else if (num2 < num3 & num3 < num1) {
-            System.out.println("Ordem dos números: " + num2 +", "+ num3 +", "+ num1);
-        } else if (num2 < num1 & num1 < num3) {
-            System.out.println("Ordem dos números: " + num2 +", " + num1 + ", " + num3);
-        } else if (num3 < num1 & num1 < num2) {
-            System.out.println("Ordem dos números: " + num3 +", "+ num1 +", "+ num2);
-        } else {
-                System.out.println("Ordem dos números: " + num3 +", "+ num2 +", "+ num1);
+        if(maior<menor){
+            aux = menor;
+            menor = maior;
+            maior = aux;
         }
+        if(maior<meio){
+            aux = meio;
+            meio = maior;
+            maior = aux;
+        }
+        if(meio<menor){
+            aux=menor;
+            menor=meio;
+            meio=aux;
+        }
+        System.out.println(menor+" "+ meio +" "+ maior);
     }
 }
